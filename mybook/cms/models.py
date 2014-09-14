06c7acce7cf5b9d9e962a9c6ed051793a42b7c86
rightgo09 <skyarrow09@gmail.com>
2014-09-14 09:28:21 +0900

    cmsにmodelを追加

--- mybook/cms/models.py
+++ mybook/cms/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    '''書籍'''
+    name = models.CharField(u'書籍名', max_length=255)
+    publisher = models.CharField(u'出版社', max_length=255, blank=True)
+    page = models.IntegerField(u'ページ数', blank=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+class Impression(models.Model):
+    '''感想'''
+    book = models.ForeignKey(Book, verbose_name=u'書籍', related_name='impressions')
+    comment = models.TextField(u'コメント', blank=True)
+
+    def __str__(self):
+        return self.comment
