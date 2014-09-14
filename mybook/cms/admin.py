697ce39108cbf97e99f6c361ef16170c31330c47
rightgo09 <skyarrow09@gmail.com>
2014-09-14 10:01:38 +0900

    admin画面で一覧表示する既存レコードの項目を変更

--- mybook/cms/admin.py
+++ mybook/cms/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from cms.models import Book, Impression
 
-admin.site.register(Book)
-admin.site.register(Impression)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'publisher', 'page')
+    list_display_links = ('id', 'name')
+admin.site.register(Book, BookAdmin)
+
+class ImpressionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comment')
+    list_display_links = ('id', 'comment')
+admin.site.register(Impression, ImpressionAdmin)
