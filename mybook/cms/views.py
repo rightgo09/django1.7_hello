0942ab824213343f19e122d2f05177509ffba3d6
rightgo09 <skyarrow09@gmail.com>
2014-09-14 11:42:12 +0900

    書籍の一覧ページ作成

--- mybook/cms/views.py
+++ mybook/cms/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from cms.models import Book
 
-# Create your views here.
+def book_list(request):
+    '''書籍の一覧'''
+    books = Book.objects.all().order_by('id')
+    return render_to_response('cms/book_list.html', {'books': books}, context_instance=RequestContext(request))
+
+def book_edit(request):
+    pass
+
+def book_del(request):
+    pass
+
